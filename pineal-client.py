4f16780ffd27adb58fe0994d35d14c3cd2b82df4
Edoardo Negri <edne@gmx.com>
2016-08-16 23:24:49 +0200

    Wait for server

--- pineal-client.py
+++ pineal-client.py
@@ -8,6 +8,8 @@ from watchdog.events import FileSystemEventHandler
 import liblo
 
 server_addr = "127.0.0.1:7172"
+listen_port = 7173
+address = liblo.Address("osc.udp://{}/".format(server_addr))
 
 
 def run(file_name):
@@ -15,7 +17,6 @@ def run(file_name):
     with open(file_name) as f:
         print("Sending {}".format(file_name))
         code = f.read()
-        address = liblo.Address("osc.udp://{}/".format(server_addr))
         liblo.send(address, "/run-code", code)
 
 
@@ -48,6 +49,22 @@ def watch(file_name):
     return observer
 
 
+def ping():
+    "Wait for server response"
+    print("Waiting for server response")
+    ack = [False]
+    server = liblo.Server(listen_port)
+
+    def ack_cb(path, args):
+        print("/ack received")
+        ack[0] = True
+
+    server.add_method("/ack", None, ack_cb)
+    while not ack[0]:
+        liblo.send(address, "/ping")
+        server.recv(100)
+
+
 def main_loop():
     "Loop forever and anlde Keyboard Interrupt"
     try:
@@ -59,6 +76,7 @@ def main_loop():
 
 if __name__ == "__main__":
     if len(argv) == 2:
+        ping()
         file_name = argv[1]
         run(file_name)
         watcher = watch(file_name)
